2ad1077549ce89057d1e56f6231144d99a7d0e1a
Andriikov10 <andreygood70@gmail.com>
2024-07-24 18:12:57 +0300

    приьрав корень квадратний

--- 3/4.cpp
+++ 3/4.cpp
@@ -2,22 +2,20 @@
 using namespace std;
 
 int main() {
-    double x1,y1,r1,x2,y2,r2,res=-999;
+    double x1, y1, r1, x2, y2, r2, res = -999;
     cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-    double dist=pow(
-        pow(abs(x1-x2),2) + pow(abs(y1-y2),2),
-        0.5
-    );
-    if(x1==x2 and y1==y2 and r1==r2)
-        res =-1;
-    else if(dist>r1+r2)
+    double dist2 = pow(x1-x2, 2) + pow(y1-y2, 2);
+    
+    if(dist2 == 0 and r1 == r2)
+        res = -1;
+    else if(dist2 > pow(r1+r2, 2))
         res = 0;
-    else if(abs(r1-r2) > dist)
+    else if(pow(r1-r2, 2) > dist2)
         res = 0;
-    else if (abs(r1+r2)==dist)
+    else if (pow(r1+r2, 2) == dist2)
         res = 1;
-    else if(abs(r2-r1)==dist)
+    else if(pow(r2-r1, 2) == dist2)
         res = 1;
-    else res=2;
+    else res = 2;
     cout << res << endl; 
 }
